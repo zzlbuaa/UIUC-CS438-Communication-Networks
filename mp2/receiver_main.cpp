87423f2aea7ef2c4462c29d294c73ebbd4335db1
Zhengliang Zhu <zz40@example.com>
2018-10-31 21:09:38 -0500

    file submission

--- mp2/receiver_main.cpp
+++ mp2/receiver_main.cpp
@@ -15,22 +15,83 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <cmath>
+
+#include <iostream>
+#include <fstream>
+
+#define MAX_BUFFER 5000
+#define MSS 1024
 
 
+typedef struct Segment{
+    short seq_num;
+    char datagram[MSS];//MSS+1?
+}segment;
+
+typedef struct Ack {
+    int seq_num;
+}ack;
 
 struct sockaddr_in si_me, si_other;
-int s, slen;
+int s;
+socklen_t slen;
+int expect_seq = 0;
+int total_packets;
+char buffer[MAX_BUFFER][MSS];
+bool isReceived[MAX_BUFFER];//need to be initialized with false
+unsigned short int senderPort;
+
+char sender[INET6_ADDRSTRLEN];//only used for printf?
+//char port[80];
 
 void diep(char *s) {
     perror(s);
     exit(1);
 }
 
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
 
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
+void response(int s_port, ack a) {
+    int fd;
+    if ((fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+        diep("ack socket");
+    si_other.sin_family = AF_INET;
+    si_other.sin_port = htons(s_port);
+
+    if (sendto(fd, &a, sizeof(a), 0, (struct sockaddr *)&si_other, sizeof(si_other)) < 0) {
+        perror("sendto failed");
+        exit(3);
+    }
+    printf("ack successful");
+}
+
+void writeFile(char* filename, unsigned long long int numBytes) {
+    //write all segments in buffer to output file
+    FILE *fp;
+    fp = fopen(filename, "w");
+    total_packets = ceil(numBytes / MSS);
+    for (int i = 0; i < total_packets; i++) {
+        fwrite(buffer[i], MSS, 1, fp);
+    }
+    fclose(fp);
+    printf("write file finished.\n");
+    return;
+}
 
 void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
     
+    bool finished = false;
     slen = sizeof (si_other);
+    int recvlen;
 
 
     if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
@@ -45,10 +106,64 @@ void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
         diep("bind");
 
 
-	/* Now receive data and send acknowledgements */    
+    /* Now receive data and send acknowledgements */    
+    while (!finished) {
+        printf("waiting for UDP on port: %d\n", myUDPport);
+        segment seg;
+        recvlen = recvfrom(s, &seg, sizeof(seg), 0, (struct sockaddr *)&si_other, &slen);
+        
+        inet_ntop(si_other.sin_family, get_in_addr((struct sockaddr *)&si_other), sender, sizeof sender);
+        printf("receiver: got udp packet from %s\n", sender);
+        if (recvlen < 0) {
+            perror("recvfrom() error");
+            exit(1);
+        }
+        if (seg.seq_num == -1) {
+            //maybe using the first packet to make sure the size is better?
+            finished = true;
+            continue;
+        }
+        if (seg.seq_num == expect_seq) {
+            ack a;
+            a.seq_num = expect_seq;
+            for (int i = 0; i < MSS; i++) {
+                buffer[seg.seq_num][i] = seg.datagram[i];
+                //if (seg.datagram[i] == '\0') {
+                //    finished = true;
+                //    break;
+                //}
+            }
+            isReceived[seg.seq_num] = true;
+            response(senderPort, a);
+            expect_seq++;
+            while (isReceived[expect_seq]) {
+                expect_seq++;
+            }
+        } else if (seg.seq_num < expect_seq) {
+            ack a;
+            a.seq_num = seg.seq_num;
+            response(senderPort, a);
+        } else {
+            isReceived[seg.seq_num] = true;
+            ack a;
+            a.seq_num = expect_seq - 1;
+            for (int i = 0; i < MSS; i++) {
+                buffer[seg.seq_num][i] = seg.datagram[i];
+                //if (seg.datagram[i] == '\0') {
+                //    finished = true;
+                //    break;
+                //}
+            }
+            //buffer[seg.seq_num] = seg.datagram;
+            response(senderPort, a);
+        }
 
+    }
+    int numbytes = 0;
+    //writefile should specify numbytes again
+    writeFile(destinationFile, numbytes);
     close(s);
-	printf("%s received.", destinationFile);
+    printf("%s received.", destinationFile);
     return;
 }
 
@@ -58,13 +173,21 @@ void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
 int main(int argc, char** argv) {
 
     unsigned short int udpPort;
+    senderPort = udpPort + 1;
 
     if (argc != 3) {
         fprintf(stderr, "usage: %s UDP_port filename_to_write\n\n", argv[0]);
         exit(1);
     }
 
+    //initialize global variables
+    for (int i = 0; i < MAX_BUFFER; i++) {
+        isReceived[i] = false;
+    }
+
     udpPort = (unsigned short int) atoi(argv[1]);
+
 
     reliablyReceive(udpPort, argv[2]);
 }
+
--- mp2/sender_main.cpp
+++ mp2/sender_main.cpp
@@ -19,28 +19,187 @@
 #include <signal.h>
 #include <string.h>
 #include <sys/time.h>
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define MSS 1024
+#define SS 0
+#define CA 1
+#define FR 2
+
+typedef struct sharedData{
+    int timeout = 10;
+    int dupAcksCount = 0;
+    int threshold = 10;
+    double CW = 1;
+    int mode = SS; //SS
+    int sendBase = 0;
+    long int timer = 0;
+    int currIdx = 0;
+    bool complete = false;
+    pthread_mutex_t mutex;
+}shared_data;
+
+typedef struct Segment{
+    short seq_num;
+    char datagram[MSS+1];
+}segment;
+
+vector<segment> buffer;
+int package_total;
+
+shared_data data;
 
 struct sockaddr_in si_other;
-int s, slen;
+struct sockaddr_in receiver_addr;
+int s;
+socklen_t addrlen = sizeof(receiver_addr);
 
-void diep(char *s) {
+void readFile(char* filename, unsigned long long int numBytes);
+
+void diep(const char *s) {
     perror(s);
     exit(1);
+}
+
+void* watchDog(void *id){
+    struct timeval tp;
+    while(true){
+        pthread_mutex_lock(&(data.mutex));
+        if(data.complete){
+            pthread_mutex_unlock(&(data.mutex));
+            return NULL;
+        }
+        gettimeofday(&tp, NULL);
+        long int ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
+        if(ms - data.timer > data.timeout){
+            cout << "time out" << endl;
+            // retrans pkt
+            segment seg = buffer[data.sendBase];
+            if(sendto(s, &seg, sizeof(seg), 0, (struct sockaddr *)&si_other, sizeof(si_other)) == -1){
+                const string msg = "send error";
+                diep(msg.c_str());
+            }
+            data.timer = ms; // update timer
+            data.threshold = data.CW / 2;
+            data.CW = 1.0;
+            data.dupAcksCount = 0;
+            data.mode = SS;
+        }
+        pthread_mutex_unlock(&(data.mutex));
+    }
+}
+
+void* receiveAck(void *id){
+    char c;
+    struct timeval tp;
+    while(true){
+        int numbytes = recvfrom(s, &c, 1, 0, (struct sockaddr *)&receiver_addr, &addrlen);
+        if(numbytes > 0){
+            pthread_mutex_lock(&(data.mutex));
+            int ack_sig = c - '0';
+            if(ack_sig == package_total-1){
+                // finish trans
+                data.complete = true;
+                pthread_mutex_unlock(&(data.mutex));
+                return NULL;
+            } 
+            cout << "ack#: " << ack_sig << endl;
+            switch(data.mode){
+                case SS: {
+                    if(ack_sig >= data.sendBase){
+                        // update window base, reset timer, 
+                        data.sendBase = ack_sig+1;
+                        gettimeofday(&tp, NULL);
+                        long int ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
+                        data.timer = ms;
+                        data.dupAcksCount = 0;
+                        data.CW += 1;
+                        if(data.CW >= data.threshold){
+                            data.mode = CA;
+                        }
+                    }else{
+                        if(ack_sig == data.sendBase-1){
+                            // fast retransmission
+                            data.dupAcksCount++;
+                            if(data.dupAcksCount == 3){
+                                // retran
+                                cout << "3 dup" << endl;
+                                segment seg = buffer[data.sendBase-1];
+                                if(sendto(s, &seg, sizeof(seg), 0, (struct sockaddr *)&si_other, sizeof(si_other)) == -1){
+                                    diep("send error");
+                                }
+                                data.mode = FR;
+                                data.threshold = data.CW / 2;
+                                data.CW = data.threshold + 3;
+                            }
+                        }
+                    }
+                    break;
+                }
+                case CA: {
+                    if(ack_sig >= data.sendBase){
+                        // update window base, reset timer, 
+                        data.sendBase = ack_sig+1;
+                        gettimeofday(&tp, NULL);
+                        long int ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
+                        data.timer = ms;
+                        data.dupAcksCount = 0;
+                        data.CW = data.CW + 1.0 / int(data.CW);
+                    }else{
+                        if(ack_sig == data.sendBase-1){
+                            data.dupAcksCount++;
+                            if(data.dupAcksCount == 3){
+                                data.mode = FR;
+                                // fast retrans
+                                cout << "3 dup" << endl;
+                                segment seg = buffer[data.sendBase-1];
+                                if(sendto(s, &seg, sizeof(seg), 0, (struct sockaddr *)&si_other, sizeof(si_other)) == -1){
+                                    diep("send error");
+                                }
+                                data.threshold = data.CW / 2;
+                                data.CW = data.threshold + 3;
+                            }
+                        }
+                    }
+                    break;
+                }
+
+                case FR: {
+                    if(ack_sig >= data.sendBase){
+                        // update window base, reset timer, 
+                        data.sendBase = ack_sig+1;
+                        gettimeofday(&tp, NULL);
+                        long int ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
+                        data.timer = ms;
+                        data.dupAcksCount = 0;
+                        data.CW = data.threshold;
+                        data.mode = CA;
+                    }else{
+                        data.CW += 1;
+                        data.dupAcksCount++;
+                    }
+                    break;
+                }
+                default:
+                    break;
+            }
+            pthread_mutex_unlock(&(data.mutex));
+        }
+    }
 }
 
 
 void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* filename, unsigned long long int bytesToTransfer) {
     //Open the file
-    FILE *fp;
-    fp = fopen(filename, "rb");
-    if (fp == NULL) {
-        printf("Could not open file to send.");
-        exit(1);
-    }
+    struct sockaddr_in si_other;
 
-    /* Determine how many bytes to transfer */
-
-    slen = sizeof (si_other);
+    // read file into buffer
+    readFile(filename, bytesToTransfer);
 
     if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
         diep("socket");
@@ -52,19 +211,70 @@ void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* file
         fprintf(stderr, "inet_aton() failed\n");
         exit(1);
     }
+    pthread_t t_a;
+    pthread_t t_b;
+    pthread_create(&t_a, NULL, &receiveAck, NULL);
+    pthread_create(&t_b, NULL, &watchDog, NULL);
 
+    while(true){
+        pthread_mutex_lock(&(data.mutex));
+        if(data.complete){
+            pthread_mutex_unlock(&(data.mutex));
+            break;
+        }
+
+        if(data.currIdx < data.sendBase + (int)data.CW){
+            segment seg = buffer[data.currIdx];
+            if(sendto(s, &seg, sizeof(seg), 0, (struct sockaddr *)&si_other, sizeof(si_other)) == -1){
+                diep("send error");
+            }
+            data.currIdx++;
+        }
+        pthread_mutex_unlock(&(data.mutex));
+    }
 
     /* Send data and receive acknowledgements on s*/
-
     printf("Closing the socket\n");
     close(s);
+
     return;
 
 }
 
-/*
- * 
- */
+void readFile(char* filename, unsigned long long int numBytes){
+    FILE *fp;
+    fp = fopen(filename, "rb");
+    if (fp == NULL) {
+        printf("Could not open file to send.");
+        exit(1);
+    }
+
+    int i=0;
+    package_total = ceil(numBytes / MSS) + 1;
+    int count = 0;
+    while(i<numBytes){
+        int j = 0;
+        segment seg;
+        seg.seq_num = count;
+        while(i<numBytes && j<1024){
+            char c = fgetc(fp);
+            seg.datagram[j] = c;
+            j++;
+            i++;
+        }
+        seg.datagram[j] = '\0';
+        buffer.push_back(seg);
+        count++;
+    }
+    segment seg;
+    seg.seq_num = -1;
+    buffer.push_back(seg);
+    fclose(fp);
+    return;
+}
+
+
+
 int main(int argc, char** argv) {
 
     unsigned short int udpPort;
@@ -77,10 +287,11 @@ int main(int argc, char** argv) {
     udpPort = (unsigned short int) atoi(argv[2]);
     numBytes = atoll(argv[4]);
 
-
+    data.mutex = PTHREAD_MUTEX_INITIALIZER;
 
     reliablyTransfer(argv[1], udpPort, argv[3], numBytes);
 
-
     return (EXIT_SUCCESS);
 }
+
+
